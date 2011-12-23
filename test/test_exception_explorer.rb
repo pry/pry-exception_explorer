a8e18c664a9cca5d58faf950627583b3a6f4fa00
John Mair <jrmair@gmail.com>
2011-12-23 19:39:21 +1300

    added some tests for more stack assertions (LazyFrame#klass)

--- test/test_exception_explorer.rb
+++ test/test_exception_explorer.rb
@@ -9,6 +9,63 @@ describe PryExceptionExplorer do
   end
 
   describe "PryExceptionExplorer#intercept" do
+    describe "class" do
+      describe "first frame" do
+        it  "should intercept exception based on first frame's method name" do
+          EE.intercept { |frame, ex| frame.klass == Toad }
+          Ratty.new.ratty
+          EE.exception_intercepted?.should == true
+        end
+
+        it  "should NOT intercept exception if method name doesn't match" do
+          EE.intercept { |frame, ex| frame.klass == Ratty }
+          begin
+            Ratty.new.ratty
+          rescue Exception => ex
+            ex.is_a?(RuntimeError).should == true
+          end
+          EE.exception_intercepted?.should == false
+        end
+      end
+
+      describe "second frame" do
+        it  "should intercept exception based on second frame's method name" do
+          EE.intercept { |frame, ex| frame.prev.klass == Weasel }
+          Ratty.new.ratty
+          EE.exception_intercepted?.should == true
+        end
+
+        it  "should NOT intercept exception if method name doesn't match" do
+          EE.intercept { |frame, ex| frame.prev.klass == Toad }
+          begin
+            Ratty.new.ratty
+          rescue Exception => ex
+            ex.is_a?(RuntimeError).should == true
+          end
+          EE.exception_intercepted?.should == false
+        end
+      end
+
+      describe "third frame" do
+        it  "should intercept exception based on third frame's method name" do
+          EE.intercept { |frame, ex| frame.prev.prev.klass == Ratty }
+          Ratty.new.ratty
+          EE.exception_intercepted?.should == true
+        end
+
+        it  "should NOT intercept exception if method name doesn't match" do
+          EE.intercept { |frame, ex| frame.prev.prev.klass == Toad }
+          begin
+            Ratty.new.ratty
+          rescue Exception => ex
+            ex.is_a?(RuntimeError).should == true
+          end
+          EE.exception_intercepted?.should == false
+        end
+      end
+
+    end
+
     describe "method_name" do
       describe "first frame" do
         it  "should intercept exception based on first frame's method name" do
@@ -63,8 +120,8 @@ describe PryExceptionExplorer do
           EE.exception_intercepted?.should == false
         end
       end
-      
+
     end
-    
+
   end
 end
