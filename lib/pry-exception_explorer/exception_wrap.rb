2f94778a57bec9e179fca63f7631d737f88e672d
John Mair <jrmair@gmail.com>
2012-01-09 01:13:07 +1300

    upgraded :when_started hook to use binding_stack, options, _pry_

--- lib/pry-exception_explorer/exception_wrap.rb
+++ lib/pry-exception_explorer/exception_wrap.rb
@@ -11,7 +11,7 @@ module PryExceptionExplorer
     self.wrap_active = true
     yield
   rescue Exception => ex
-    Pry.config.hooks.add_hook(:when_started, :setup_exception_context) do |binding_stack, _pry_|
+    Pry.config.hooks.add_hook(:when_started, :setup_exception_context) do |binding_stack, options, _pry_|
       binding_stack.replace([ex.exception_call_stack.first])
       PryStackExplorer.create_and_push_frame_manager(ex.exception_call_stack, _pry_)
       PryStackExplorer.frame_manager(_pry_).user[:exception] = ex
