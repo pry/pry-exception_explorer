9c8e6fa74864cf13223323af49ad624648a74d74
John Mair <jrmair@gmail.com>
2012-02-07 16:46:45 +1300

    added specs for raise to ensure we havent mangled it

--- pry-exception_explorer.gemspec
+++ pry-exception_explorer.gemspec
@@ -2,19 +2,19 @@
 
 Gem::Specification.new do |s|
   s.name = "pry-exception_explorer"
-  s.version = "0.1.3.2"
+  s.version = "0.1.3.5"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["John Mair (banisterfiend)"]
-  s.date = "2012-02-06"
+  s.date = "2012-02-07"
   s.description = "Enter the context of exceptions"
   s.email = "jrmair@gmail.com"
-  s.files = [".gemtest", ".gitignore", ".travis.yml", ".yardopts", "CHANGELOG", "Gemfile", "LICENSE", "README.md", "Rakefile", "examples/example_c_inline.rb", "examples/example_inline.rb", "examples/example_wrap.rb", "lib/pry-exception_explorer.rb", "lib/pry-exception_explorer/cli.rb", "lib/pry-exception_explorer/commands.rb", "lib/pry-exception_explorer/core_ext.rb", "lib/pry-exception_explorer/intercept.rb", "lib/pry-exception_explorer/lazy_frame.rb", "lib/pry-exception_explorer/shim_builder.rb", "lib/pry-exception_explorer/version.rb", "pry-exception_explorer.gemspec", "test/helper.rb", "test/test_exceptions_in_pry.rb", "test/test_inline_exceptions.rb", "test/test_wrapped_exceptions.rb"]
+  s.files = [".gemtest", ".gitignore", ".travis.yml", ".yardopts", "CHANGELOG", "Gemfile", "LICENSE", "README.md", "Rakefile", "examples/example_c_inline.rb", "examples/example_inline.rb", "examples/example_wrap.rb", "lib/pry-exception_explorer.rb", "lib/pry-exception_explorer/cli.rb", "lib/pry-exception_explorer/commands.rb", "lib/pry-exception_explorer/core_ext.rb", "lib/pry-exception_explorer/intercept.rb", "lib/pry-exception_explorer/lazy_frame.rb", "lib/pry-exception_explorer/shim_builder.rb", "lib/pry-exception_explorer/version.rb", "pry-exception_explorer.gemspec", "test/helper.rb", "test/test_exceptions_in_pry.rb", "test/test_inline_exceptions.rb", "test/test_raise.rb", "test/test_wrapped_exceptions.rb"]
   s.homepage = "https://github.com/banister/pry-exception_explorer"
   s.require_paths = ["lib"]
   s.rubygems_version = "1.8.11"
   s.summary = "Enter the context of exceptions"
-  s.test_files = ["test/helper.rb", "test/test_exceptions_in_pry.rb", "test/test_inline_exceptions.rb", "test/test_wrapped_exceptions.rb"]
+  s.test_files = ["test/helper.rb", "test/test_exceptions_in_pry.rb", "test/test_inline_exceptions.rb", "test/test_raise.rb", "test/test_wrapped_exceptions.rb"]
 
   if s.respond_to? :specification_version then
     s.specification_version = 3
---
+++ test/test_raise.rb
@@ -0,0 +1,132 @@
+require 'helper'
+
+describe PryExceptionExplorer do
+
+  describe "normal raise behaviour (EE.enabled = false)" do
+    before do
+      EE.enabled = false
+    end
+
+    it 'should raise the exception that was raised' do
+      begin
+        raise ArgumentError
+      rescue => ex
+        ex.is_a?(ArgumentError).should == true
+      end
+    end
+
+    it 'should keep message' do
+      begin
+        raise ArgumentError, "hello"
+      rescue => ex
+        ex.message.should == "hello"
+      end
+    end
+
+    it 'should implicitly re-raise rescued exceptione' do
+      begin
+        raise ArgumentError
+      rescue => ex
+        begin
+          raise
+        rescue => ex2
+          ex2.is_a?(ArgumentError).should == true
+        end
+      end
+    end
+
+    it 'should implicitly raise RuntimeError (when not inside rescue)' do
+      begin
+        raise
+      rescue => ex
+        ex.is_a?(RuntimeError).should == true
+      end
+    end
+
+    it 'should re-raise raised rescued exception' do
+      begin
+        raise ArgumentError
+      rescue => ex
+        begin
+          raise ex
+        rescue
+          ex.is_a?(ArgumentError).should == true
+        end
+      end
+    end
+  end
+
+  describe "raise behaviour AFTER interception (EE.enabled = true)" do
+    before do
+      Pry.config.hooks.add_hook(:when_started, :save_caller_bindings, WhenStartedHook)
+      Pry.config.hooks.add_hook(:after_session, :delete_frame_manager, AfterSessionHook)
+
+      EE.enabled = true
+      EE.intercept(ArgumentError)
+      EE.wrap_active = false
+      Pry.config.input = StringIO.new("exit-all\n")
+      # Pry.config.input = Readline
+      # Pry.config.output = $stdout
+    end
+
+    after do
+      Pry.config.hooks.delete_hook(:when_started, :save_caller_bindings)
+      Pry.config.hooks.delete_hook(:after_session, :delete_frame_manager)
+    end
+
+    it 'should raise the exception that was raised' do
+      begin
+        raise ArgumentError
+      rescue => ex
+        ex.is_a?(ArgumentError).should == true
+      end
+    end
+
+    it 'should keep message' do
+      begin
+        raise ArgumentError, "hello"
+      rescue => ex
+        ex.message.should == "hello"
+      end
+    end
+
+    it 'should implicitly re-raise rescued exceptione' do
+      begin
+        raise ArgumentError
+      rescue => ex
+        begin
+          raise
+        rescue => ex2
+          ex2.is_a?(ArgumentError).should == true
+        end
+      end
+    end
+
+    it 'should implicitly raise RuntimeError (when not inside rescue)' do
+      begin
+        raise
+      rescue => ex
+        ex.is_a?(RuntimeError).should == true
+      end
+    end
+
+    it 'should re-raise raised rescued exception' do
+      begin
+        raise ArgumentError
+      rescue => ex
+        begin
+          raise ex
+        rescue
+          ex.is_a?(ArgumentError).should == true
+        end
+      end
+    end
+
+
+
+
+
+  end
+end
+
+
