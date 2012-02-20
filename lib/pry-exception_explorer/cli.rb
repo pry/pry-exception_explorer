41f1e76728331caf94b3b09dc561dbbcd1d5accc
John Mair <jrmair@gmail.com>
2012-02-20 21:55:45 +1300

    version 0.1.8, fixed `-w` option
    * also now defining `raise` monkeypatch inside a module and including into Object
      (rather that defining on Object directly)

--- lib/pry-exception_explorer/cli.rb
+++ lib/pry-exception_explorer/cli.rb
@@ -1,6 +1,7 @@
 Pry::CLI.add_options do
 
-  on :w, :wrap, "Run the script wrapped by the exception explorer", true do |file|
+  on :w, :wrap, "Run the provided FILE wrapped by the exception explorer", true do |file|
+    require 'pry-exception_explorer'
     PryExceptionExplorer.wrap do
       require file
     end
